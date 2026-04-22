257fbbfdf658a251e6465528775ac6ca5f63192e
Julian Dario Gonzalez Toledo <juliant.2001@outlook.com>
2026-04-21 22:21:54 -0500

    chore: Se consultan el total de inventarios diarios y el producto más ingresado en salidas de inventarios

--- inventory/views.py
+++ inventory/views.py
@@ -32,6 +32,19 @@ def inventory_entry_view(request):
 @login_required(login_url='inventory_auth:login')
 def inventory_outs_view(request):
     inventarios = TaInventario.objects.all().filter(tipoInventario=2)
+    total_inventarios = TaInventario.objects.filter(
+        tipoInventario=2,
+        fechaRegistro__date=date.today()
+    ).count()
+    producto_top_stat = TaInventario.objects.filter(tipoInventario=2).values('producto').annotate(
+        cantidad=Count('producto')
+    ).order_by('-cantidad').first()
+    if producto_top_stat:
+        producto_top = TaProducto.objects.get(id=producto_top_stat['producto'])
+    else:
+        producto_top = None
     return render(request, 'inventory/inventory-outs.html', {
-        'inventarios': inventarios
+        'inventarios': inventarios,
+        'total_inventarios': total_inventarios,
+        'producto_top': producto_top
     })
